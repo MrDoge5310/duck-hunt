237ad449c1a228c096fff3e31eb47506f66bb61e
LOGIKA <danilltrotsenko2002@gmail.com>
2024-01-06 12:08:01 +0200

    moving

--- characters.py
+++ characters.py
@@ -11,8 +11,9 @@ class Sprite:
         self.hitbox = pygame.Rect(x, y, self.size, self.size)
         self.image = pygame.image.load(filename)
         self.image = pygame.transform.scale(self.image, (self.size, self.size))
-        self.vx = 5
-        self.vy = 5
+        self.vx = random.randint(3, 6)
+        self.vy = random.randint(3, 6)
+        self.canGoAway = False
 
     def draw(self, wnd):
         wnd.blit(self.image, (self.hitbox.x, self.hitbox.y))
@@ -23,15 +24,19 @@ class Sprite:
 
         # ограничиваем движение утки пределами игровой области
         if self.hitbox.x < 0:
-            self.hitbox.x = 0
-            self.vx *= -1
+            if not self.canGoAway:
+                self.hitbox.x = 0
+                self.vx *= -1
         elif self.hitbox.x > 800 - self.size:
-            self.hitbox.x = 800 - self.size
-            self.vx *= -1
+            if not self.canGoAway:
+                self.hitbox.x = 800 - self.size
+                self.vx *= -1
 
         if self.hitbox.y < 0:
-            self.hitbox.y = 0
-            self.vy *= -1
+            if not self.canGoAway:
+                self.hitbox.y = 0
+                self.canGoAway = True
+                self.vy *= -1
         elif self.hitbox.y > 400 - self.size:
             self.hitbox.y = 400 - self.size
             self.vy *= -1
